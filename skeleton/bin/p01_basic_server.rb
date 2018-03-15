6b2da1e8afbe453279e243944fb1165b4bfd8f3e
Steven Choi <steven.m.choi@gmail.com>
2018-03-14 17:08:16 -0700

    Created basic Rails Lite server

--- skeleton/bin/p01_basic_server.rb
+++ skeleton/bin/p01_basic_server.rb
@@ -1,1 +1,25 @@
 require 'rack'
+require "byebug"
+
+app = Proc.new do |env|
+  # debugger
+  req = Rack::Request.new(env)
+
+  if req.path_info == '/'
+    path_text = 'Hello world!'
+  else
+    path_text = req.path_info
+  end
+
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  res.write(path_text)
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
+
+# TODO: localhost:3000/i/love/app/academy
